ecd63a69de1002205585ca73fa89b83b855663a4
it24103583 <it24103583@my.sliit.lk>
2025-09-17 18:40:28 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24103583.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\raviru\\Desktop\\IT24103583")
+dbinom(40,44,0.92)
+pbinom(35,44,0.92,lower.tail = TRUE)
+1-pbinom(37,44,0.92,lower.tail = TRUE)
+pbinom(37,44,0.92,lower.tail = FALSE)
+pbinom(42,44,0.92,lower.tail = TRUE)-pbinom(39,44,0.92,lower.tail = TRUE)
+
+1-pbinom(46,50,0.85,lower.tail = TRUE)
+pbinom(46,50,0.85,lower.tail = FALSE)
+
+dpois(15,12)
+
